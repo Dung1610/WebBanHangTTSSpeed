d900b2ac3d0961247b65b796f441bfdd8f57cdf5
Dung1610 <nguyentand1610@gmail.com>
2024-08-10 00:16:23 +0700

    update

--- src/screenes/shippingMethods/index.jsx
+++ src/screenes/shippingMethods/index.jsx
@@ -1,9 +1,4 @@
-import {
-  Box,
-  Typography,
-  useTheme,
-  Button,
-} from "@mui/material";
+import { Box, Typography, useTheme, Button, TextField } from "@mui/material";
 import * as React from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
@@ -30,6 +25,43 @@ import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
+import Stack from "@mui/material/Stack";
+import {
+  Unstable_NumberInput as BaseNumberInput,
+  numberInputClasses,
+} from "@mui/base/Unstable_NumberInput";
+import { styled } from "@mui/system";
+
+const NumberInput = React.forwardRef(function CustomNumberInput(props, ref) {
+  return (
+    <BaseNumberInput
+      slots={{
+        root: StyledInputRoot,
+        input: StyledInputElement,
+        incrementButton: StyledButton,
+        decrementButton: StyledButton,
+      }}
+      slotProps={{
+        incrementButton: {
+          children: "▴",
+        },
+        decrementButton: {
+          children: "▾",
+        },
+      }}
+      {...props}
+      ref={ref}
+    />
+  );
+});
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const style = {
   position: "absolute",
@@ -70,11 +102,12 @@ const ShippingMethods = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [shippingMethods, setShippingMethods] = useState([]);
+  const [newShippingMethods, setNewShippingMethods] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [openModal, setOpenModal] = React.useState(false);
   const handleOpen = () => setOpenModal(true);
   const handleClose = () => setOpenModal(false);
-
+  const [value, setValue] = React.useState(null);
   const [position, setPosition] = useState(null);
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -108,7 +141,7 @@ const ShippingMethods = () => {
     const map = useMap();
     React.useEffect(() => {
       if (position) {
-        map.setView([position.lat, position.lng], 11); // Cập nhật vị trí và zoom level
+        map.setView([position.lat, position.lng]); // Cập nhật vị trí và zoom level
       }
     }, [position, map]);
     return null;
@@ -226,7 +259,7 @@ const ShippingMethods = () => {
           <MapContainer
             center={[21.0283334, 105.854041]}
             zoom={10}
-            style={{ height: "70vh", width: "70vh" }}
+            style={{ height: "40vh", width: "100vh" }}
           >
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -243,6 +276,54 @@ const ShippingMethods = () => {
               </Marker>
             )}
           </MapContainer>
+          <TextField
+            id="fullWidth"
+            variant="filled"
+            size="medium"
+            required
+            fullWidth
+            onChange={(e) => setNewShippingMethods(e.target.value)}
+          />
+          <Stack
+              direction="row"
+              justifyContent="space-evenly"
+              alignItems="center"
+              spacing={0}
+          >
+            <Item>
+              <Typography variant="h5" textAlign={"center"} component="div">
+                Tên Danh Mục:
+              </Typography>
+              <NumberInput
+                aria-label="Demo number input"
+                placeholder="Type a number…"
+                value={value}
+                onChange={(event, val) => setValue(val)}
+              />
+            </Item>
+            <Item>
+            <Typography variant="h5" textAlign={"center"} component="div">
+                Tên Danh Mục:
+              </Typography>
+              <NumberInput
+                aria-label="Demo number input"
+                placeholder="Type a number…"
+                value={value}
+                onChange={(event, val) => setValue(val)}
+              />
+            </Item>
+            <Item>
+            <Typography variant="h5" textAlign={"center"} component="div">
+                Tên Danh Mục:
+              </Typography>
+              <NumberInput
+                aria-label="Demo number input"
+                placeholder="Type a number…"
+                value={value}
+                onChange={(event, val) => setValue(val)}
+              />
+            </Item>
+          </Stack>
           <CardActions>
             <Button size="large" color="inherit">
               <Typography gutterBottom variant="h5" component="div">
@@ -308,3 +389,144 @@ const ShippingMethods = () => {
 };
 
 export default ShippingMethods;
+
+const blue = {
+  100: "#DAECFF",
+  200: "#80BFFF",
+  400: "#3399FF",
+  500: "#007FFF",
+  600: "#0072E5",
+};
+
+const grey = {
+  50: "#F3F6F9",
+  100: "#E5EAF2",
+  200: "#DAE2ED",
+  300: "#C7D0DD",
+  400: "#B0B8C4",
+  500: "#9DA8B7",
+  600: "#6B7A90",
+  700: "#434D5B",
+  800: "#303740",
+  900: "#1C2025",
+};
+
+const StyledInputRoot = styled("div")(
+  ({ theme }) => `
+  font-family: 'IBM Plex Sans', sans-serif;
+  font-weight: 400;
+  border-radius: 8px;
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+  box-shadow: 0px 2px 2px ${
+    theme.palette.mode === "dark" ? grey[900] : grey[50]
+  };
+  display: grid;
+  grid-template-columns: 1fr 19px;
+  grid-template-rows: 1fr 1fr;
+  overflow: hidden;
+  column-gap: 8px;
+  padding: 4px;
+
+  &.${numberInputClasses.focused} {
+    border-color: ${blue[400]};
+    box-shadow: 0 0 0 3px ${
+      theme.palette.mode === "dark" ? blue[600] : blue[200]
+    };
+  }
+
+  &:hover {
+    border-color: ${blue[400]};
+  }
+
+  // firefox
+  &:focus-visible {
+    outline: 0;
+  }
+`
+);
+
+const StyledInputElement = styled("input")(
+  ({ theme }) => `
+  font-size: 0.875rem;
+  font-family: inherit;
+  font-weight: 400;
+  line-height: 1.5;
+  grid-column: 1/2;
+  grid-row: 1/3;
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  background: inherit;
+  border: none;
+  border-radius: inherit;
+  padding: 8px 12px;
+  outline: 0;
+`
+);
+
+const StyledButton = styled("button")(
+  ({ theme }) => `
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  appearance: none;
+  padding: 0;
+  width: 19px;
+  height: 19px;
+  font-family: system-ui, sans-serif;
+  font-size: 0.875rem;
+  line-height: 1;
+  box-sizing: border-box;
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 0;
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 120ms;
+
+  &:hover {
+    background: ${theme.palette.mode === "dark" ? grey[800] : grey[50]};
+    border-color: ${theme.palette.mode === "dark" ? grey[600] : grey[300]};
+    cursor: pointer;
+  }
+
+  &.${numberInputClasses.incrementButton} {
+    grid-column: 2/3;
+    grid-row: 1/2;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    border: 1px solid;
+    border-bottom: 0;
+    &:hover {
+      cursor: pointer;
+      background: ${blue[400]};
+      color: ${grey[50]};
+    }
+
+  border-color: ${theme.palette.mode === "dark" ? grey[800] : grey[200]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : grey[50]};
+  color: ${theme.palette.mode === "dark" ? grey[200] : grey[900]};
+  }
+
+  &.${numberInputClasses.decrementButton} {
+    grid-column: 2/3;
+    grid-row: 2/3;
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border: 1px solid;
+    &:hover {
+      cursor: pointer;
+      background: ${blue[400]};
+      color: ${grey[50]};
+    }
+
+  border-color: ${theme.palette.mode === "dark" ? grey[800] : grey[200]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : grey[50]};
+  color: ${theme.palette.mode === "dark" ? grey[200] : grey[900]};
+  }
+  & .arrow {
+    transform: translateY(-1px);
+  }
+`
+);

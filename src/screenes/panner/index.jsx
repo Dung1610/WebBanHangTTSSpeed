238b2804441fed3a45a4c379dfb914ee5aec8088
Dung1610 <nguyentand1610@gmail.com>
2024-08-10 19:03:54 +0700

    update banner

--- src/screenes/panner/index.jsx
+++ src/screenes/panner/index.jsx
@@ -20,35 +20,7 @@ import { myAxios } from "../../Services/axios";
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Modal from "@mui/material/Modal";
-import { styled } from "@mui/material/styles";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 500,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
+import LinearProgress from '@mui/material/LinearProgress';
 
 const Panner = () => {
   CheckExpired();
@@ -58,6 +30,7 @@ const Panner = () => {
   const [banner,setBanner] = useState([])
   const [file, setFile] = useState(null);
   const [fetchTrigger, setFetchTrigger] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const handleRefresh = () => {
     setFetchTrigger(prev => !prev);
   };
@@ -74,6 +47,7 @@ const Panner = () => {
         },
       })
       .then(function (response) {
+        setIsLoading(false)
         console.log(response.data)
         setBanner(response.data)
       })
@@ -83,18 +57,21 @@ const Panner = () => {
   };
 
   const deleteBanner = (code) =>{
+    setIsLoading(true)
     myAxios
-      .get("banners/delete",{
-        "images": [`${code}`]
+      .post("banners/delete",{
+        images: [`${code}`]
       }, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
       })
       .then(function (response) {
+        setIsLoading(false)
         handleRefresh()
       })
       .catch((error) => {
+        setIsLoading(false)
         console.error("Error:", error);
       });
    }
@@ -112,17 +89,19 @@ const Panner = () => {
           Thêm Banner
         </Typography>
       </Button>
-      <ImageList sx={{ width: '100%', height: '75vh' }} cols={4} rowHeight={'auto'} gap={30}>
+      {isLoading ? <LinearProgress color="secondary" /> : null}
+      <ImageList sx={{ width: '100%', height: '100%' }} cols={4} rowHeight={'auto'} gap={30} loading={true}>
       {banner.map((item) => (
         <ImageListItem key={item.id}>
           <img
             src={`${item.name}`.includes("http") ? item.name :`http://localhost:5181/api/get/image/${item.name}`}
+            loading="lazy"
           />
           <ImageListItemBar
             title={item.id}
             actionIcon={
               <Button variant="contained" onClick={() => deleteBanner(item.id)} color="success">
-              <Typography variant="h6" color="#fff">
+              <Typography variant="h4" color="#fff">
                 Xoá
               </Typography>
           </Button>
